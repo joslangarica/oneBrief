cd88d1e0bb14ffe217b669cab244e5c03e7834ae
joslangarica <joslangarica@gmail.com>
2023-05-02 19:26:38 +0700

    working really good, preview with 3 sizes, prompt getting better this is before improving uxui

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def generate_website_files(brief):
 
     # Generate HTML content
     print("Generating HTML content...")
-    html_prompt = f"write an optimized html script that follows SEO and UX best practices for the following brief: {brief} You SHOULD ONLY WRITE THE HTML CODE with clear id, labels, and sections but only HTML no other output"
+    html_prompt = f"  create an HTML file based on the provided brief,  the CSS will be written separately after the HTML is delivered so write the html considering that(include the link rel stylesheet to style.css). the HTML code should declare the appropiate doctype, incorporate the HTML tags (h1...h6), metadata, set appropiate title tags, images should have alt attribute, descriptive meta attributes, with the proper document structure, group relevant sections, Include all necessary elements (tags, titles, headers, alt attributes, paragraphs, images, etc.) for SEO Best Practices, if necessary Use image placeholders from [https://source.unsplash.com](https://source.unsplash.com/) . Remember that your answer should only contain the HTML code with no additional comments from you. {brief}"
     html_content = generate_content(html_prompt, api_key)
 
     # Generate CSS content using the HTML content
     print("Generating CSS content...")
-    css_prompt = f"write a responsive, modern look css file for the following html structure: {brief}"
+    css_prompt = f"Please analyze the provided html and write CSS responsive alligned to the brief (make sure the nav adapts accordingly). Your CSS code should: Be well-commented, it should work and display properly on mobile, desktop and tablet.Include basic animations or dynamic. Use image placeholders from [https://source.unsplash.com](https://source.unsplash.com/) if necessary. Remember that your answer should only contain the CSS code with no additional comments from you {brief}"
     css_content = generate_content(css_prompt, api_key, conversation_history=[
         {"role": "user", "content": html_prompt},
         {"role": "assistant", "content": html_content}
