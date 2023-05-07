6123bc01e53558278013996778b92c11ca5c60fa
joslangarica <joslangarica@gmail.com>
2023-05-07 13:46:27 +0700

    gpt4 with some prompt modifications

--- generator.py
+++ generator.py
@@ -2,14 +2,17 @@ import openai
 
 def generate_content(prompt, api_key, conversation_history=None):
     try:
-        messages = [{"role": "user", "content": prompt}]
+        messages = [
+            {"role": "system", "content": "You are a senior web developer, expert in Bootstrap and web content creation"},
+            {"role": "user", "content": prompt}]
         if conversation_history:
             messages = conversation_history + messages
 
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4",
+            #model="gpt-3.5-turbo",
             messages=messages,
-            temperature=0.7,
+            temperature=1,
             api_key=api_key
         )
         return response.choices[0]["message"]["content"]
